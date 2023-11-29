d15c797fa5b7ca95aa4272ed4d493fef098f29a0
sumit23802380 <sumitjangid1692153@gmail.com>
2023-11-29 17:21:13 +0530

    Added : Initialized Repository

---
+++ src/com/bridgelabz/binarysearchtree/INode.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.binarysearchtree;
+
+public interface INode <T extends Comparable<T>>{
+}
---
+++ src/com/bridgelabz/binarysearchtree/MyBinaryNode.java
@@ -0,0 +1,2 @@
+package com.bridgelabz.binarysearchtree;public class MyBinaryNode {
+}
